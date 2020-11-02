66294a8052837749d6d83143d85600c30867903b
Chris Kalama <chriskalama96@gmail.com>
2020-11-01 23:52:48 -0600

    Completed delete in CRUD methodology for burger_controller.jsfile

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -45,7 +45,17 @@ router.put("/api/burgers/:id", function(req, res){
     });
 });
 
-router.delete("/api/cats/:id", )
+router.delete("/api/cats/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.delete(condition, function(result){
+        if(result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 
 
