1da8791a288c6df2b4ea7675885f939478d58c83
Chris Kalama <chriskalama96@gmail.com>
2020-11-01 23:37:26 -0600

    Added a router.post function with logic

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -16,3 +16,12 @@ router.get("/", function(req,res){
         res.render("index", hbsObject);
     });
 });
+router.post("/api/burgers", function (req, res){
+    burger.create([
+        "burger_name" , "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result){
+        res.json({ id: result.insertId})
+    });
+});
