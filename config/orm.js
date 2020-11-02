c3cf4d81bcdbf63d2335622c1b76559680957ea1
Chris Kalama <chriskalama96@gmail.com>
2020-11-01 21:00:23 -0600

    Created CRUD layout (omitting DELETE) in orm.js

--- config/orm.js
+++ config/orm.js
@@ -29,18 +29,67 @@ function objToSql(ob) {
           arr.push(key + "=" + value);
         }
       }
+
+      return arr.toString();
 }
 
+//CRUD SQL STATEMENT FUNCTIONS
+var orm = {
+    //To grab any inputs
+    all:function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        })
+    },
+    //To create a new column with values 
+    create:function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function (err,result) {
+        if(err) {
+            throw err
+        }
+
+        cb(result);
+
+    })
+
+    },
+    // An example of objColVals would be {burger_name: Greek, devoured: true}
+    update: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition; 
+
+        connection.query(queryString, function(err, result) {
+            if(err) {
+                throw err;
+            }
+            cb(result);
+        })
+
+  }
 
 
 
 
-
-
-
-
-
-
+}
 
 
 module.exports = orm; 
