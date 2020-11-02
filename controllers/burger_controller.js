8ba53c518efddf0bb5e03c1747ec1199a96c75eb
Chris Kalama <chriskalama96@gmail.com>
2020-11-01 23:26:26 -0600

    Created first router.get function

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -6,4 +6,13 @@ var router = express.Router();
 //Importing Burger.js (Which contains the burger model) and uses its database functions.
 var burger = require("../models/burger.js");
 
-//
+//Setting up routes with logic
+router.get("/", function(req,res){
+    burger.all(function(data){
+        var hbsObject = {
+            burgers:data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
