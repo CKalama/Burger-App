0a7cff1f0d3e2a28ba7c7a7bfcdaff97a3ca0443
Chris Kalama <chriskalama96@gmail.com>
2020-11-01 20:18:07 -0600

    Set up database within mysql and created a db folder

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burgers_db; 
+USE burgers_db;
+
+CREATE TABLE burgers (
+id INT NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(30),
+devoured BOOLEAN,
+PRIMARY KEY(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers
+VALUES (1, "Blue Cheese", false);
+
+INSERT INTO burgers
+VALUES(2, "Pepper Jack", false);
+
+
+INSERT INTO burgers
+VALUES(3, "American", false); 
