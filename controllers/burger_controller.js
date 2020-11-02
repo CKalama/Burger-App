0592e2992643281fca183f1fe0ea8972ab6860c4
Chris Kalama <chriskalama96@gmail.com>
2020-11-01 23:46:54 -0600

    Created router.put logic with a burger.update function on devoured value in db

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -16,12 +16,37 @@ router.get("/", function(req,res){
         res.render("index", hbsObject);
     });
 });
+
 router.post("/api/burgers", function (req, res){
     burger.create([
         "burger_name" , "devoured"
     ], [
         req.body.burger_name, req.body.devoured
     ], function(result){
+        // Send back the ID of the new quote
         res.json({ id: result.insertId})
     });
 });
+
+router.put("/api/burgers/:id", function(req, res){
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured:req.body.devoured
+    }, condition, function(result){
+        if (result.changedRows == 0) {
+            //If no rows were changed, it will send back a 404 because ID must not exist. 
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/cats/:id", )
+
+
+
+module.exports = router;
