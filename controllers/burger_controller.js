1e83f363324c25de4dccc1743344cee73a5ee150
Chris Kalama <chriskalama96@gmail.com>
2020-11-01 23:59:00 -0600

    fixed error in burger_controller.js on line 48

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -45,7 +45,7 @@ router.put("/api/burgers/:id", function(req, res){
     });
 });
 
-router.delete("/api/cats/:id", function (req, res) {
+router.delete("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
 
     burger.delete(condition, function(result){
